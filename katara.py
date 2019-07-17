38dbd0a92101dcec7655cb3980a5ca9cbd9305c7
Jeremy Bilic <jeremy.bilic@gmail.com>
2019-07-17 16:01:32 +0200

    Fixed a bug
    Nulls would count as matching values which caused columns to pass the matching threshold and have its errors counted

--- katara.py
+++ katara.py
@@ -18,8 +18,8 @@ def load_file(domin_specific_file, domain_specific_types, rel2sub2obj):
             rel2sub2obj[rel] = {s: o}
 
 
-def domain_spec_col_type(data, i, domain_specific_types, col_2_errors_repair):
-    values = [row[i] for row in data]
+def domain_spec_col_type(data, col, domain_specific_types, col_2_errors_repair):
+    values = [row[col] for row in data]
 
     lowercase_types = {domain_type.lower() for domain_type in domain_specific_types}
 
@@ -27,11 +27,11 @@ def domain_spec_col_type(data, i, domain_specific_types, col_2_errors_repair):
     tempdict = {}
     for index in range(len(data)):
         value = values[index]
-        if value.lower() in lowercase_types:
+        if value.lower() in lowercase_types and value.lower() != '':
             count += 1
         else:
             fix = ""
-            tempdict[(index, i)] = fix
+            tempdict[(index, col)] = fix
 
     coverage = count/len(values)
 
@@ -49,13 +49,13 @@ def domain_spec_colpair(data, i, j, rel2sub2obj, col_2_errors_repair):
             coli = row[i]
             colj = row[j]
             if coli in rel2sub2obj[rel]:
-                if colj == rel2sub2obj[rel][coli]:
+                if coli != '' and colj == rel2sub2obj[rel][coli]:
                     count += 1
                 else:
                     repair_value = rel2sub2obj[rel][coli]
                     tempdict[(index, j)] = repair_value
             if colj in rel2sub2obj[rel]:
-                if coli == rel2sub2obj[rel][colj]:
+                if colj != '' and coli == rel2sub2obj[rel][colj]:
                     back_count += 1
                 else:
                     repair_value = rel2sub2obj[rel][colj]
@@ -69,7 +69,6 @@ def domain_spec_colpair(data, i, j, rel2sub2obj, col_2_errors_repair):
         if backcoverage >= 0.15:
             col_2_errors_repair.update(backdict)
 
-
 def run_katara(data, domin_specific_file):
     domain_specific_types = set()
     rel2sub2obj = {}
@@ -77,8 +76,8 @@ def run_katara(data, domin_specific_file):
 
     load_file(domin_specific_file, domain_specific_types, rel2sub2obj)
 
-    for i in range(len(data[0])):
-        domain_spec_col_type(data, i, domain_specific_types, col_2_errors_repair)
+    for col in range(len(data[0])):
+        domain_spec_col_type(data, col, domain_specific_types, col_2_errors_repair)
 
     for i in range(len(data[0])):
         for j in range(len(data[0])-1, i, -1):
