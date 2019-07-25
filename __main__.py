40e25b652a200ccddb466a9b6223e28b2e7b44ed
Jeremy Bilic <jeremy.bilic@gmail.com>
2019-07-25 11:33:36 +0200

    Corrected path for test datasets

--- __main__.py
+++ __main__.py
@@ -5,8 +5,8 @@ import os
 DATASET_NAME = "flights"
 dataset_dictionary = {
     "name": DATASET_NAME,
-    "path": os.path.join("datasets", DATASET_NAME, "dirty.csv"),
-    "clean_path": os.path.join("datasets", DATASET_NAME, "clean.csv")
+    "path": os.path.join(os.path.dirname(__file__), "datasets", DATASET_NAME, "dirty.csv"),
+    "clean_path": os.path.join(os.path.dirname(__file__), "datasets", DATASET_NAME, "clean.csv")
 }
 
 application = raha.Raha(dataset_dictionary)
