cb8c452e507cda7d1b24810ede98c97fe9c5bf8e
Mohammad <moh.mahdavi.l@gmail.com>
2019-08-06 14:46:43 +0200

    updating gitignore.

--- build/lib/raha/raha_required/tools/dBoost/dboost/features/utils.py
+++
@@ -1,4 +0,0 @@
-import unicodedata
-
-def string_normalize(s): # http://stackoverflow.com/questions/517923/
-   return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
