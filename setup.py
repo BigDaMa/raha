92513580a36509933469d33d84705801dd66db9d
Mohammad <moh.mahdavi.l@gmail.com>
2020-11-11 12:04:47 +0100

    creating a pypi repository.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="raha",
-    version="1.21",
+    version="1.23",
     packages=["raha"],
     url="https://github.com/bigdama/raha",
     license="Apache 2.0",
@@ -13,5 +13,5 @@ setup(
     install_requires=["numpy", "pandas", "scipy", "sklearn", "matplotlib", 
     "prettytable", "mwparserfromhell", "beautifulsoup4", "py7zr"],
     include_package_data=True,
-    download_url="https://github.com/BigDaMa/raha/archive/v1.22.tar.gz"
+    download_url="https://github.com/BigDaMa/raha/archive/v1.23.tar.gz"
 )
