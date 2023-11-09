6f3d5e7ccd45a514bb97f5b4a719f2f0f7fa869f
Malte Kuhlmann <m.f.kuhlmann@gmail.com>
2023-11-09 16:44:54 +0100

    updated init

--- raha/__init__.py
+++ raha/__init__.py
@@ -6,3 +6,7 @@ from .sequential.utilities import *
 from .sequential.benchmark import *
 from .sequential.tools.KATARA.katara import *
 from .sequential.tools.dBoost.dboost.imported_dboost import *
+
+from .parallel.dataset_parallel import *
+from .parallel.tools.KATARA.katara import *
+from .parallel.tools.dBoost.dboost.imported_dboost import *
